c8efdbc4a93f47e0388e1ed16d2900938f0c7932
manoj agarwal <aryan070303@gmail.com>
2022-09-22 02:08:10 +0530

    user defined room

--- public/js/main.js
+++ public/js/main.js
@@ -4,6 +4,7 @@ const chatForm = document.getElementById('chat-form');
 const chatMessages = document.querySelector('.chat-messages'); 
 const roomName = document.getElementById('room-name');
 const userList = document.getElementById('users');
+const newRoom = document.getElementById('newRoom');
 
 const {username,room} = Qs.parse(location.search, {
     ignoreQueryPrefix:true
@@ -54,3 +55,4 @@ function outputUsers(users) {
     ${users.map(user => `<li>${user.username}</li>`).join('')}
     `;
 }
+
